ff667350bbe3ff7ff7117298b07a5028d3af9875
Antonio Robson de Paula <robson.depaula@gmail.com>
2020-06-24 12:07:38 -0300

    Solved the max pairwise algorithm.

--- week1_programming_challenges/2_maximum_pairwise_product/MaxPairwiseProduct.java
+++ week1_programming_challenges/2_maximum_pairwise_product/MaxPairwiseProduct.java
@@ -3,17 +3,20 @@ import java.io.*;
 
 public class MaxPairwiseProduct {
     static int getMaxPairwiseProduct(int[] numbers) {
-        int max_product = 0;
-        int n = numbers.length;
-
-        for (int first = 0; first < n; ++first) {
-            for (int second = first + 1; second < n; ++second) {
-                max_product = Math.max(max_product,
-                    numbers[first] * numbers[second]);
+        // instead of multiplying each one and then finding the max:
+        // 1st = find the two largest numbers
+        // 2nd = multiply the two largest and return the value
+        int largest_1 = Integer.MIN_VALUE, largest_2 = Integer.MIN_VALUE;
+        for (int value : numbers) {
+            if (value > largest_1) {
+                largest_2 = largest_1;
+                largest_1 = value;
+            } else if (value > largest_2) {
+                largest_2 = value;
             }
         }
 
-        return max_product;
+        return largest_1 * largest_2;
     }
 
     public static void main(String[] args) {
@@ -32,8 +35,7 @@ public class MaxPairwiseProduct {
 
         FastScanner(InputStream stream) {
             try {
-                br = new BufferedReader(new
-                    InputStreamReader(stream));
+                br = new BufferedReader(new InputStreamReader(stream));
             } catch (Exception e) {
                 e.printStackTrace();
             }
