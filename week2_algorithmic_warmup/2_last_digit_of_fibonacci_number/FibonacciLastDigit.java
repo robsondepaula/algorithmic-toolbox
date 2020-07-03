b92bee7d812ddd83a7f80350e8535d986f7a0182
Antonio Robson de Paula <robson.depaula@gmail.com>
2020-07-03 17:24:19 -0300

    Result of week 2 exercises.

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
@@ -6,7 +6,7 @@ public class FibonacciLastDigit {
             return n;
 
         int previous = 0;
-        int current  = 1;
+        int current = 1;
 
         for (int i = 0; i < n - 1; ++i) {
             int tmp_previous = previous;
@@ -16,12 +16,25 @@ public class FibonacciLastDigit {
 
         return current % 10;
     }
-    
+
+    public static int getFibonacciLastDigitNonNaive(int n) {
+        if (n <= 1)
+            return n;
+
+        int[] result = new int[n + 1];
+        result[0] = 0;
+        result[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            result[i] = (result[i - 1] + result[i - 2]) % 10;
+        }
+        return result[n];
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int c = getFibonacciLastDigitNaive(n);
+        //int c = getFibonacciLastDigitNaive(n);
+        int c = getFibonacciLastDigitNonNaive(n);
         System.out.println(c);
     }
 }
-
--- week2_algorithmic_warmup/4_least_common_multiple/LCM.java
+++ week2_algorithmic_warmup/4_least_common_multiple/LCM.java
@@ -9,11 +9,27 @@ public class LCM {
     return (long) a * b;
   }
 
+  private static long euclidGcd(long a, long b) {
+    long divisor = a >= b ? a : b;
+    long dividend = a <= b ? a : b;
+    while (divisor != 0) {
+      long remainder = dividend % divisor;
+      dividend = divisor;
+      divisor = remainder;
+    }
+    return dividend;
+  }
+
+  private static long lmcNonNaive(long a, long b) {
+    return (a * b) / euclidGcd(a, b);
+  }
+
   public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
     int a = scanner.nextInt();
     int b = scanner.nextInt();
 
-    System.out.println(lcm_naive(a, b));
+    // System.out.println(lcm_naive(a, b));
+    System.out.println(lmcNonNaive(a, b));
   }
 }
